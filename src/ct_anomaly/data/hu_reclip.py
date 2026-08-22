b0918f334bc1c0522379986cb35a71cb77e932c3
Fatemeh Hosseini <fh.hosseini2001@gmail.com>
2026-08-22 13:51:21 +0200

    feat: add GPU HU reclipping

---
+++ src/ct_anomaly/data/hu_reclip.py
@@ -0,0 +1,8 @@
+import torch
+
+
+def reclip_hu(volume_tensor, original_hu_min, original_hu_max, new_hu_min, new_hu_max):
+    print(f"DEBUG: reclip_hu called! range going to [{new_hu_min}, {new_hu_max}]")
+    hu = (volume_tensor + 1) / 2 * (original_hu_max - original_hu_min) + original_hu_min
+    hu_clipped = torch.clamp(hu, new_hu_min, new_hu_max)
+    return 2 * (hu_clipped - new_hu_min) / (new_hu_max - new_hu_min) - 1
